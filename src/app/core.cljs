90dad5c1663463f9b86d4c38d824dd50b1080706
Brunya <zbruno.linux@gmail.com>
2020-05-22 12:04:06 +0200

    ackee http post

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,6 +1,7 @@
 (ns app.core
   (:require [reagent.core :as r]
-            [app.views :as views]))
+            [app.views :as views]
+            [app.peekaboo :as peekaboo]))
 
 (defn ^:dev/after-load start
   []
@@ -8,4 +9,6 @@
 
 (defn ^:export main
   []
-  (start))
+  (do
+    (start)
+    (peekaboo/gift "d572dd2c-8ffd-4ef0-b7f0-9ec5f58e5e0e")))
---
+++ src/app/peekaboo.cljs
@@ -0,0 +1,18 @@
+(ns app.peekaboo
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :refer [<!]]
+            [cljs-http.client :as http]))
+
+(defn gift [id]
+  (go (let [lang (-> js/navigator .-language)
+            data {:browserName (-> js/navigator .-appName)
+                  ;:siteReferrer "https://example.com/referrer.html"
+                  :siteLocation (-> js/window .-location .-hostname)
+                  :osName (-> js/navigator .-platform)
+                  :browserHeight (-> js/screen .-height)
+                  :browserWidth (-> js/screen .-width)
+                  :deviceManufacturer (case (< (-> js/screen .-width) 768) "mobile" (= (-> js/screen .-width) 768) "tablet" (> (-> js/screen .-width) 768) "desktop")
+                  :screenHeight (-> js/screen .-height)
+                  :screenWidth (-> js/screen .-width)
+                  :browserVersion (-> js/navigator .-appVersion)}
+            post (<! (http/post (str "https://analytics.zegen.org/domains/" id "/records") {:json-params data :with-credentials? false :headers {"Content-Type" "application/json"}}))])))
