ef23cd49587ca2d1fa968f33c87b55d4bf53daf0
marcelllaks <marcelllakos@gmail.com>
2020-07-24 17:23:50 +0200

    peekaboo update

--- src/app/peekaboo.cljs
+++ src/app/peekaboo.cljs
@@ -15,30 +15,34 @@
     (includes? userAgent "Firefox") "Firefox"
     (includes? userAgent "Chrome") "Chrome"
     (includes? userAgent "Safari") "Safari"
-    (or (includes? userAgent "OPR") (includes? userAgent "Opera")) "Opera"
-    (or (includes? userAgent "MSIE") (includes? userAgent "Trident")) "Internet Explorer"
-    (includes? userAgent "Edg") "Microsoft Edge"
+    (or (includes? userAgent "OPR") (includes? userAgent "Opera")) "Opr"
+    (or (includes? userAgent "MSIE") (includes? userAgent "Trident")) "MSIE"
+    (includes? userAgent "Edg") "Edg"
     :else "Unknown browser")))
 
-(defn gift [id]
-  (let [newuser (atom {:key false})]
-   (when (nil? (.getItem (.-localStorage js/window) "zgen")) ((reset! newuser {:key true})(.setItem (.-localStorage js/window) "zgen" id)))
-   (swap! app-state assoc (keyword (.getItem (.-localStorage js/window) "zgen")) {:newuser (str @newuser)
-                                                                                  :browserName (browser-checker)
-                                                                                  :siteLocation (-> js/window .-location .-hostname)
-                                                                                  :osName (-> js/navigator .-platform)
-                                                                                  :cpuCores (-> js/navigator .-hardwareConcurrency)
-                                                                                  :deviceManufacturer (case (< (-> js/screen .-width) 768) "mobile" (= (-> js/screen .-width) 768) "tablet" (> (-> js/screen .-width) 768) "desktop")
-                                                                                  :screenHeight (-> js/screen .-height)
-                                                                                  :screenWidth (-> js/screen .-width)
-                                                                                  :cookies? (-> js/navigator .-cookieEnabled)
-                                                                                  :cookies (-> js/document .-cookie)
-                                                                                  :colorDepth (-> js/screen .-colorDepth)
-                                                                                  :pixelDepth (-> js/screen .-pixelDepth)
-                                                                                  :pathName (-> js/window .-location .-pathname)
-                                                                                  :clientTime (.Date js/window)
-                                                                                  :referrer (-> js/document .-referrer)
-                                                                                  :prevSites (-> js/history .-length)
-                                                                                  :protocol (-> js/window .-location .-protocol)
-                                                                                  :browserLang (-> js/navigator .-language)
-                                                                                  :time (.getTime (js/Date.))})))
+(defn gift [timestamp]
+  (let [newuser (atom true)
+        cookie? (-> js/navigator .-cookieEnabled)]
+    (when cookie?
+      (if (nil? (-> js/window .-localStorage (.getItem "id"))) (.setItem (.-localStorage js/window) "id" timestamp) (reset! newuser false)))
+    (let [id (keyword (if cookie? (.getItem (.-localStorage js/window) "id") (.getTime (js/Date.))))]
+      (swap! data assoc id
+       {:newuser @newuser
+        :browserName (browser-checker)
+        :siteLocation (-> js/window .-location .-hostname)
+        :osName (-> js/navigator .-platform)
+        :cpuCores (-> js/navigator .-hardwareConcurrency)
+        :deviceManufacturer (cond (< (-> js/screen .-width) 768) "mobile" (= (-> js/screen .-width) 768) "tablet" (> (-> js/screen .-width) 768) "desktop")
+        :screenHeight (-> js/screen .-height)
+        :screenWidth (-> js/screen .-width)
+        :cookies? (-> js/navigator .-cookieEnabled)
+        :cookies (-> js/document .-cookie)
+        :colorDepth (-> js/screen .-colorDepth)
+        :pixelDepth (-> js/screen .-pixelDepth)
+        :pathName (-> js/window .-location .-pathname)
+        :clientTime (.Date js/window)
+        :referrer (-> js/document .-referrer)
+        :prevSites (-> js/history .-length)
+        :protocol (-> js/window .-location .-protocol)
+        :browserLang (-> js/navigator .-language)
+        :time (.getTime (js/Date.))}))))
